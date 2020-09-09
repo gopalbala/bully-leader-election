fda7d57679e9093c379ff8aa0fca652f7f09e4df
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-10 02:29:15 +0530

    updated discovery

--- src/main/java/com/gb/bullyelection/CommunicationService.java
+++ src/main/java/com/gb/bullyelection/CommunicationService.java
@@ -14,15 +14,15 @@ import java.util.List;
 public class CommunicationService {
 
     private Member self;
-    private ServerSocket socket;
-    private byte[] receivedBuffer = new byte[1024];
-    private DatagramPacket receivePacket =
-            new DatagramPacket(receivedBuffer, receivedBuffer.length);
+    private ServerSocket serverSocket;
+//    private byte[] receivedBuffer = new byte[1024];
+//    private DatagramPacket receivePacket =
+//            new DatagramPacket(receivedBuffer, receivedBuffer.length);
 
     public CommunicationService(Member member, int portToListen) throws IOException {
         try {
             this.self = member;
-            socket = new ServerSocket(portToListen);
+            serverSocket = new ServerSocket(portToListen);
         } catch (SocketException e) {
             System.out.println("Could not create socket connection");
             e.printStackTrace();
@@ -55,7 +55,7 @@ public class CommunicationService {
 
     public void membershipAdditions(Member clusterMember) throws IOException, ClassNotFoundException {
 
-       // ServerSocket serverSocket = new ServerSocket(self.getDiscoveryPort());
+        // ServerSocket serverSocket = new ServerSocket(self.getDiscoveryPort());
 //        while (true) {
 //            Socket socket = serverSocket.accept();
 
@@ -63,35 +63,85 @@ public class CommunicationService {
 
         if (clusterMember == null) {
             System.out.println("ready to receive members");
-            ObjectInputStream memRequest = new ObjectInputStream(socket.accept().getInputStream());
-            ObjectOutputStream memResponse = new ObjectOutputStream(socket.accept().getOutputStream());
-            Member member = (Member) memRequest.readObject();
-            self.getPeers().putIfAbsent(member.getId(), member);
-            memResponse.writeObject(self.getPeers().values());
-            notifyMemberAddition(member);
+
+            self.getPeers().putIfAbsent(self.getId(), self);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                ObjectInputStream memRequest = new
+                        ObjectInputStream(socket.getInputStream());
+                Member member = (Member) memRequest.readObject();
+//                memRequest.close();
+                System.out.println("Discovered new member " + member.getId());
+                if (member.getId() != self.getId()) {
+                    self.getPeers().putIfAbsent(member.getId(), member);
+//                    ObjectOutputStream memResponse = new
+//                            ObjectOutputStream(socket.getOutputStream());
+//                    memResponse.writeObject(self);
+//                    memResponse.close();
+                    for (Member m : self.getPeers().values()) {
+//                        if (m.getId()!=member.getId()) {
+                        try {
+//                            Socket skt = new Socket("127.0.0.1", self.getDiscoveryPort());
+                            ObjectOutputStream memResponse = new
+                                    ObjectOutputStream(socket.getOutputStream());
+                            memResponse.writeObject(m);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        //memResponse.writeObject(m);
+                    }
+
+//                    }
+                }
+                //notifyMemberAddition(member);
+            }
         } else {
             System.out.println("Registering to cluster");
-            Socket clSock = new Socket("localhost", clusterMember.getDiscoveryPort());
+            Socket clSock = new Socket("127.0.0.1", clusterMember.getDiscoveryPort());
             ObjectOutputStream memResponse = new ObjectOutputStream(clSock.getOutputStream());
             memResponse.writeObject(self);
-            ObjectInputStream memRequest = new ObjectInputStream(socket.accept().getInputStream());
-            Member member = (Member) memRequest.readObject();
-            self.getPeers().putIfAbsent(member.getId(), member);
-            memResponse.writeObject(self.getPeers().values());
-            notifyMemberAddition(member);
+            new Thread(() -> {
+                try {
+                    this.receiver(clSock);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+//            while (true) {
+//                Socket socket = serverSocket.accept();
+
+                //memResponse.writeObject(self.getPeers().values());
+                //;
+//            }
         }
-        System.out.println();
+        // System.out.println();
 //        }
     }
 
-    private void notifyMemberAddition(Member newMember) throws IOException {
-        for (Member member : self.getPeers().values()) {
-            if (self.getId() != member.getId()) {
-                ServerSocket serverSocket = new ServerSocket(self.getDiscoveryPort());
-                System.out.println("ready to receive members");
-                ObjectInputStream memRequest = new ObjectInputStream(socket.accept().getInputStream());
-                ObjectOutputStream memResponse = new ObjectOutputStream(socket.accept().getOutputStream());
-            }
+    private void receiver(Socket socket) throws IOException, ClassNotFoundException {
+        while (true) {
+            ObjectInputStream memRequest = new ObjectInputStream(socket.getInputStream());
+            Member member = (Member) memRequest.readObject();
+            System.out.println("Discovered new member " + member.getId());
+            if (member.getId() != self.getId())
+                self.getPeers().putIfAbsent(member.getId(), member);
         }
     }
+
+    private void selfReceiver() {
+
+    }
+
+//    private void notifyMemberAddition(Member newMember) throws IOException {
+//        for (Member member : self.getPeers().values()) {
+//            if (self.getId() != member.getId()) {
+//                Socket socket = serverSocket.accept();
+//                System.out.println("ready to receive members");
+//                ObjectInputStream memRequest = new ObjectInputStream(this.serverSocket.accept().getInputStream());
+//                ObjectOutputStream memResponse = new ObjectOutputStream(this.serverSocket.accept().getOutputStream());
+//            }
+//        }
+//    }
 }
