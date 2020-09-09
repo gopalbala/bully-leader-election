2ff930e196cf7eeb56f2cc9a382846beee2a3333
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-09 23:55:24 +0530

    Added BullyElectionMain

---
+++ src/main/java/com/gb/bullyelection/BullyElectionMain.java
@@ -0,0 +1,47 @@
+package com.gb.bullyelection;
+
+import lombok.SneakyThrows;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.time.Duration;
+
+public class BullyElectionMain {
+    public static void main(String[] args) throws IOException {
+        ElectionConfig electionConfig = new ElectionConfig(
+                Duration.ofSeconds(5),
+                Duration.ofSeconds(3),
+                Duration.ofSeconds(5));
+
+        Member member = new Member(new InetSocketAddress("127.0.0.1", 19000),
+                3,19001,19002,19003);
+
+        Member member1 = new Member(new InetSocketAddress("127.0.0.1", 19100),
+                3,19101,19102,19103);
+
+        CommunicationService communicationService =
+                new CommunicationService(member1,19103);
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    communicationService.membershipAdditions(member);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try {
+            thread.start();
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
