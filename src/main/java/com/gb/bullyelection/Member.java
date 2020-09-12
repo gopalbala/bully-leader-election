271658cfb1f04d585c0bfd314ecd57a002aeeab2
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-12 19:04:16 +0530

    Updated data port

--- src/main/java/com/gb/bullyelection/Member.java
+++ src/main/java/com/gb/bullyelection/Member.java
@@ -20,19 +20,21 @@ public class Member implements Serializable {
     private final int electionPort;
     private final int keepAlivePort;
     private final int discoveryPort;
+    private final int dataPort;
     private boolean coordinator;
     @Setter
     private Map<Integer,Member> peers;
     public Member(String host, int id,
                   int port,
                   int electionPort,
-                  int keepAlivePort, int discoveryPort) {
+                  int keepAlivePort, int discoveryPort, int dataPort) {
         this.host = host;
         this.id = id;
         this.port = port;
         this.electionPort = electionPort;
         this.keepAlivePort = keepAlivePort;
         this.discoveryPort = discoveryPort;
+        this.dataPort = dataPort;
         peers = new HashMap<>();
     }
 }
